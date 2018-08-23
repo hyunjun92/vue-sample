4d20d4e34ca0271f9d37e4a7e01e87825fbe96fc
linkd2ev <m89824@ncgn.co.kr>
2018-08-23 11:33:17 +0900

    todo-app 기본 프레임/CSS 작업

---
+++ vue-todo-app/src/components/todoApp.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="form-group">
+      <h1>Vue To-Do
+        <small>List</small>
+      </h1>
+      <form role="form">
+        <input type="text" class="form-control" placeholder="해야 할 일" name="task">
+      </form>
+      <button type="button" class="btn btn btn-primary">Add</button>
+    </div>
+    <div></div>
+    <ul class="list-unstyled" id="todo"></ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todoApp'
+}
+</script>
+
+<style scoped>
+form {
+    display: inline-block;
+}
+.form-group {
+    text-align: center;
+    margin: 10em;
+    padding-bottom: 25px;
+}
+#todo {
+    margin: 0 auto;
+    width: 500px;
+}
+a.close {
+  float: right;
+}
+</style>
